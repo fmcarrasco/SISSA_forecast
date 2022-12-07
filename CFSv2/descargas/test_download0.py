8f4473732fb3b8da5f11dd77b31139ed3b8f0425
Félix Carrasco <felix.marcial@gmail.com>
2022-12-07 16:39:18 -0300

    CFSv2

---
+++ CFSv2/descargas/test_download0.py
@@ -0,0 +1,16 @@
+# 1. Import the requests library
+import requests
+from tqdm import tqdm
+
+URL = "https://www.ncei.noaa.gov/data/climate-forecast-system/access/operational-9-month-forecast/time-series/2011/201112/20111212/2011121200/prate.01.2011121200.daily.grb2"
+#URL2 = "https://www.ncei.noaa.gov/data/climate-forecast-system/access/reforecast/6-hourly-flux-9-month-runs/2006/200612/20061212/flxf2007020212.01.2006121200.grb2"
+# 2. download the data behind the URL
+response = requests.get(URL)
+total = int(response.headers.get('content-length', 0))
+# 3. Open the response into a new file called instagram.ico
+# open("acpcp_sfc_2000010500_c00.grib2", "wb").write(response.content)
+fname = "prate.01.2011121200.daily.grb2"
+with open(fname, 'wb') as file, tqdm(desc=fname, total=total, unit='iB', unit_scale=True, unit_divisor=1024) as bar:
+        for data in response.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
